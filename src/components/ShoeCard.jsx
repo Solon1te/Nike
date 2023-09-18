7d55bf9ab399148bb6c5c61b0860fe1ccf7f4596
Mark Dietrich <MarkADietr@gmail.com>
2023-09-18 19:10:17 -0400

    create shoecard and hero section work

---
+++ src/components/ShoeCard.jsx
@@ -0,0 +1,25 @@
+
+const shoeCard = ({imgURL, changeBigShoeImage, bigShoeImg}) => {
+    const handleClick = () => {
+        if(bigShoeImg !== imgURL.bigShoe)
+        {
+            changeBigShoeImage(imgURL.bigShoe)
+        }
+    } 
+  return (
+    <div className={`border-2 rounded-xl ${bigShoeImg === imgURL
+    ? 'border-coral-red'
+    : 'border-transparent'
+    } cursor-pointer max-sm:flex-1 `}
+    onClick={handleClick}
+    >
+        <div>
+            <img src={imgURL.thumbnail}
+            alt="" />
+        </div>
+
+    </div>
+  )
+}
+
+export default shoeCard
